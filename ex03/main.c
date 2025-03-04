87b5c78aac56fd6103ff61402ed55936a3661aef
Kilian Pouliquen <kipouliq@made-f0Dr3s6.clusters.42paris.fr>
2025-03-04 17:46:57 +0100

    ex02 ex03 done

--- ex03/main.c
+++ ex03/main.c
@@ -6,7 +6,7 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 11:49:24 by kipouliq          #+#    #+#             */
-/*   Updated: 2025/03/04 16:28:27 by kipouliq         ###   ########.fr       */
+/*   Updated: 2025/03/04 17:46:39 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,16 @@ int main ()
 {
     DDRB = 0b00000001; // DDRx : controls i/o of 8 pins of the microcontroller (B: pins 0-7)
                         // set input or output to the corresponding bit (here : pin 0)
-    int state = 0;
     while (1)
     {
-        if ((PIND & (1 << PD2)) == 0) // PINx : checking state of said pin (0 == switch pressed)
+        if ((PIND & (1 << PD2)) == 0)   // PINx : checking state of said pin (0 == switch pressed)
         {
-            _delay_ms(50); // avoid rebound effect : wait for the switch signal to be stable
-            if ((PIND & (1 << PD2)) && !state) // check if the button has been released (PINx == 1)
-                state = 1;
-            else if ((PIND & (1 << PD2)) && state)
-                state = 0;
+            _delay_ms(50);  // avoid rebound effect : wait for the switch signal to be stable
+            if ((PIND & (1 << PD2)) == 0) // and checking again
+            {
+                PORTB ^= (1 << PB0);    // flipping the led bit 
+                while ((PIND & (1 << PD2)) == 0) {} // blocking execution while button is pressed (avoid blinking led)
+            }
         }
-        if (state)
-            PORTB |= 0b00000001;
-        if (!state)
-            PORTB = 0x00;
     }
 }
