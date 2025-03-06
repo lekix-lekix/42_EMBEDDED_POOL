1840f6078cc5548631effe535410b16bb904269d
Kilian Pouliquen <kipouliq@made-f0Dr3s6.clusters.42paris.fr>
2025-03-06 19:00:57 +0100

    ex01 done

--- module02/ex01/main.c
+++ module02/ex01/main.c
@@ -5,13 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/06 14:01:03 by kipouliq          #+#    #+#             */
-/*   Updated: 2025/03/06 17:21:23 by kipouliq         ###   ########.fr       */
+/*   Created: 2025/03/06 16:34:19 by kipouliq          #+#    #+#             */
+/*   Updated: 2025/03/06 17:47:55 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+void uart_printstr(const char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        while (!(UCSR0A & (1 << UDRE0))) {}
+        UDR0 = str[i];
+        i++;
+    }
+}
+
+ISR(TIMER1_COMPA_vect, ISR_BLOCK)
+{
+    uart_printstr("Hello World!\r\n");
+    reti();
+}
 
 void uartinit(void)
 {
@@ -22,19 +41,23 @@ void uartinit(void)
 
 void uart_tx(unsigned char c)
 {
-    while (!(UCSR0A & (1 << UDRE0))) {} // checking that no tranmission is ongoing, by checking the "Data Register Empty" flag
+    while (!(UCSR0A & (1 << UDRE0))) {} // checking that no transmission is ongoing, by checking the "Data Register Empty" flag
     UDR0 = c; // loading the data buffer with our char
+}
+
+void init_timer()
+{
+    OCR1A = 3124; // setting PWM resolution, aka when to trigger an event
+    ICR1 = 62499; // setting TOP value, aka full phase of the PWM
+    TIMSK1 = (1 << OCIE1A);
+    TCCR1A = (1 << WGM11);
+    TCCR1B = (1 << WGM13 | 1 << WGM12 | 1 << CS12); // toggling fast PWM mode 14 with 256 prescaler
 }
 
 int main ()
 {
+    sei();
+    init_timer();
     uartinit();
-    while (1)
-    {
-        uart_tx('Z');
-        _delay_ms(1000);
-    }
+    while (1) {}
 }
-
-// Baud rate register calculation : (cpu_freq / (16 * baud rate)) - 1
-// here : (16 000 000 / (16 * 115200)) - 1 = 7.6805.. rounded at 8 
