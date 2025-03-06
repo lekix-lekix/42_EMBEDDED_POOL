e8f974368e83e7470cdc92b7015d7366b98e518c
Kilian Pouliquen <kipouliq@made-f0Dr3s6.clusters.42paris.fr>
2025-03-06 16:33:56 +0100

    ex00 done

---
+++ module02/ex00/main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 14:01:03 by kipouliq          #+#    #+#             */
+/*   Updated: 2025/03/06 16:33:41 by kipouliq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void uartinit(void)
+{
+    UBRR0L = 8; // initializing the baud rate low register value
+    UBRR0H = 0x00; // as the value is < 255, no need to set anything in the high register
+    UCSR0B = (1 << TXEN0); // setting the "Transmit Enabled" bit in the UCSR0B register
+}
+
+void uart_tx(unsigned char c)
+{
+    while (!(UCSR0A & (1 << UDRE0))) {} // checking that no tranmission is ongoing, by checking the "Data Register Empty" flag
+    UDR0 = c; // loading the data buffer with our char
+}
+
+int main ()
+{
+    uartinit();
+    while (1)
+    {
+        uart_tx('Z');
+        _delay_ms(1000);
+    }
+}
+
+
+// Baud rate register calculation : (cpu_freq / (16 * baud rate)) - 1
+// here : (16 000 000 / (16 * 115200)) - 1 = 7.6805.. rounded at 8 
